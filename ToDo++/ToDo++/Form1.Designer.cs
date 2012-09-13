3027598db065869be4fa8fe320070ce364ed75b5
Raaj <Raaj@Raaj-PC.comp.nus.edu.sg>
2012-09-13 15:50:32 +0800

    GUI Added

---
+++ ToDo++/ToDo++/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ToDo__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toDoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outputText = new System.Windows.Forms.RichTextBox();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toDoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toDoToolStripMenuItem
+            // 
+            this.toDoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.toDoToolStripMenuItem.Name = "toDoToolStripMenuItem";
+            this.toDoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.toDoToolStripMenuItem.Text = "ToDo++";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(12, 27);
+            this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
+            this.outputText.Size = new System.Drawing.Size(260, 191);
+            this.outputText.TabIndex = 1;
+            this.outputText.Text = "";
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(13, 230);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(195, 20);
+            this.inputText.TabIndex = 2;
+            this.inputText.Text = "press alt q lah, or type stuff here press enter";
+            this.inputText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputText_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toDoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox outputText;
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ ToDo++/ToDo++/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ToDo__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Set Formatting for your Text
+        void AppendText(RichTextBox box, Color color, string text)
+        {
+            int start = box.TextLength;
+            box.AppendText(text);
+            int end = box.TextLength;
+
+            // Textbox may transform chars, so (end-start) != text.Length
+            box.Select(start, end - start + 1);
+            box.SelectionColor = color;
+            // could set box.SelectionBackColor, box.SelectionFont, etc...
+            box.SelectionLength = 0; // clear
+        }
+
+        //Append the RTB
+        void DisplayCommand(string text)
+        {
+            AppendText(outputText, Color.Red, "ToDo++: ");
+            AppendText(outputText, Color.Black, text);
+            AppendText(outputText, Color.Red, "\n");
+            inputText.Text = "";
+        }
+
+        //Button Clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayCommand(inputText.Text);
+        }
+
+        //Press Enter
+        private void inputText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                DisplayCommand(inputText.Text);
+            }
+        }
+
+        //Minise to Tray with ovverride for shortcutkey
+        private void minimiseToTray(bool shortCutPressed)
+        {
+            notifyIcon1.BalloonTipTitle = "Minimize to Tray App";
+            notifyIcon1.BalloonTipText = "You have successfully minimized your form.";
+
+            if (FormWindowState.Minimized == this.WindowState || shortCutPressed==true)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        //When we hit minimize, it goes to tray, you can choose to remove this
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            //minimiseToTray(false);
+        }
+
+        //Double click the tray icon and it pops back up (Need a way to have a shortcut invoke the app again)
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            minimiseToTray(false);
+        }
+
+        //Pressing ALT+Q Will Minimize the app to the tray
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Alt | Keys.Q))
+            {
+                System.Diagnostics.Trace.WriteLine("Pressed");
+                minimiseToTray(true);
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        //Paramters to load when Form Opens.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
