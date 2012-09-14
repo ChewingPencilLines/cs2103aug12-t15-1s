d97854afdc494a7721403730152c12979e71247f
Raaj <Raaj@Raaj-PC.nus.edu.sg>
2012-09-14 14:37:06 +0800

    Processing.h Now Commited

---
+++ ToDo++/ToDo++/Processing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo__
+{
+    public class Processing
+    {
+        public string returnOutput(string input)
+        {
+            return string.Format("return {0}", input);
+        }
+    }
+}
