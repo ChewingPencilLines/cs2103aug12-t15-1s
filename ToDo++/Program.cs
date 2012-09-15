f5a7d54f74646b89bc5e8b685bd12d3536957a57
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-09-15 17:43:52 +0800

    Initialize

---
+++ ToDo++/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for test
+            Interface a =new Interface();
+        }
+    }
+}
