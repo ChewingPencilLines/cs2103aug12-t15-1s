44bc8cc871a6c8b095a9fff5e00ca0378dc99534
Raaj <Raaj@Raaj-PC>
2012-09-18 00:22:46 +0800

    Added ClassDiagram and respective classes related. Added Task Objects.

---
+++ ToDo++/ToDo++/TimeTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class TimeTask : Task
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+    }
+}
