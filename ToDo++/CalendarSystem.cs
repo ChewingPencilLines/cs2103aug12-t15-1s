b8655adaa1346b867cbef7c36ad411d694c2bf4a
Raaj <Raaj@Raaj-PC>
2012-09-18 00:33:44 +0800

    Added Class Diagram and Task Files. See Class Diagram for more details

---
+++ ToDo++/CalendarSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ToDo
+{
+    class CalendarSystem
+    {
+        Calendar systemCalendar = CultureInfo.InvariantCulture.Calendar;    //Make use of Calendars functions
+        DateTime myDT = new DateTime(2002, 4, 3, new GregorianCalendar());
+
+    }
+}
---
+++ ToDo++/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    abstract class Task
+    {
+        protected TaskType taskID;
+        protected string taskName;
+
+        public TaskType TaskType
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
