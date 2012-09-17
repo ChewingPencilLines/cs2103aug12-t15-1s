ae5a38eb883c315aa76778b0a1217fb2efd1911c
Raaj <Raaj@Raaj-PC>
2012-09-18 00:33:44 +0800

    Added Class Diagram and Task Files. See Class Diagram for more details

---
+++ ToDo++/TaskList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class TaskList:List<Task>
+    {
+        internal Task Task
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+
+    class TaskListDelegate
+    {
+        public TaskList TaskList
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
