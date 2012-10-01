bb3742ff982a4c942829da57a9c4686ca66298b7
Ivan <ravenxce@gmail.com>
2012-10-01 21:57:07 +0800

    @ivan to all: MERGE CAREFULLY! - finished the searchtime function -- not tested for bugs! - refactored / moved methods within/between classes - broke two tests. this is the test fault due to white box testing,  not the code. will clean when available.

---
+++ ToDo++/Logic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class Logic
+    {
+        private Stack<string> CommandStack = new Stack<string>();
+
+        public Logic()
+        {
+
+        }
+
+        public string ProcessCommand(string input)
+        {
+            string command = ValidateCommand(input);
+            Operation op = DecomposeCommand(command);
+            return ExecuteCommand(op);
+        }
+
+        private string ExecuteCommand(Operation OP)
+        {
+            string result = "";
+            if (OP.Equals(null))
+            {
+                return null;
+            }
+            else
+            {
+                //@todo: pass op to crud part and get result;
+                return result;
+            }
+        }
+
+        private Operation DecomposeCommand(string command)
+        {
+            if (command.Equals(null))
+            {
+                return null;
+            }
+            else
+            {
+                CommandParser CP = new CommandParser();
+                return CP.ParseOperation(command);
+            }
+        }
+
+        private string ValidateCommand(string command)
+        {
+            if (!command.Equals(null) & command.Length == 0)
+            {
+                return null;
+            }
+            else
+            {
+                CommandStack.Push(command);
+                return command;
+            }
+        }
+
+    }
+}
---
+++ ToDo++/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ToDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command, feedback;
+            Logic AI = new Logic();
+
+            command = Console.ReadLine();       
+            feedback = AI.ProcessCommand(command);
+            Console.WriteLine(feedback);
+        }
+    }
+}
