9b6d834ab8a33a69ff60d951421d6831fc79e45d
Ivan <ravenxce@gmail.com>
2012-10-06 12:50:01 +0800

    @ivan: made a sample combined numeric regex with freespacing.

--- ToDo++/StringParser.cs
+++ ToDo++/StringParser.cs
@@ -49,8 +49,7 @@ namespace ToDo
         // matches dd-mm-yyyy, d-m-yyyy, dd-mm-yy or d-m-yy
         static Regex date_numericDMYFormat =
             new Regex(@"\b(?<day>(0?[1-9]|[12][0-9]|3[01]))?([-/.])(?<month>(0[1-9]|1[012]))\2(?<year>(?:(20)?)\d\d)\b");
-
-
+        
         // matches mm-dd-yyyy, m-d-yyyy, mm-dd-yy, m-d-yy
         static Regex date_numericMDYFormat =
             new Regex(@"\b(?<month>(0?[1-9]|1[012]))([-/.])(?<day>(0[1-9]|[12][0-9]|3[01]))\2(?<year>(?:(20)?)\d\d)\b");
@@ -72,6 +71,26 @@ namespace ToDo
         // matchess yyyy mm dd
         static Regex date_alphabeticYMDFormat =
             new Regex(@"\b(?<year>(?:(20)?)\d\d)\s(?<month>(jan(?:(uary))?|feb(?:(ruary))?|mar(?:(ch))?|apr(?:(il))?|may|jun(?:e)?|jul(?:y)?|aug((?:ust))?|sep((?:t|tember))?|oct((?:ober))?|nov((?:ember))?|dec((?:ember))?))\s(?<day>(([123]?[1])|([12]?[2])|([12]?[3])|([12]?[4-9])|([123][0])))");
+
+
+        static Regex date_numericFormat =
+            new Regex(@"\b
+                        # DD/MM
+                        (
+                        ((?<day>(0?[1-9]|[12][0-9]|3[01]))
+                        ?([-/.])
+                        (?<month>(0[1-9]|1[012])))
+                        |
+                        # MM/DD
+                        ((?<month>(0[1-9]|1[012]))
+                        ?([-/.])
+                        (?<day>(0?[1-9]|[12][0-9]|3[01]))
+                        )
+                        # (YY)YY
+                        ?([-/.])
+                        (?<year>(?:(\d\d)?)\d\d)
+                        \b"
+                    , RegexOptions.IgnorePatternWhitespace);
 
         static StringParser()
         {
@@ -442,7 +461,6 @@ namespace ToDo
                 index++;
             }
             return tokens;
-            throw new NotImplementedException();
         }
         
         /// <summary>
