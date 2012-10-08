17b2381d0ead24d397e2badf7585c3bff705b037
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-10-08 10:58:13 +0800

    @alice move undo&redo stack to logic

---
+++ ToDo++/Logic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class Logic
+    {
+        OperationHandler operationHandler; 
+        private Stack<Operation> undoStack;
+        private Stack<Operation> redoStack;
+
+        public Logic()
+        {
+            operationHandler = new OperationHandler();
+        }
+
+        public string ProcessCommand(string input)
+        {
+            //string command = ValidateCommand(input);
+            Operation operation = ParseCommand(input);
+            undoStack.Push(operation);
+            return ExecuteCommand(operation);
+        }
+
+        public bool UpdateSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExecuteCommand(Operation operation)
+        {
+            if (operation.Equals(null))
+            {
+                return null;
+            }
+            else
+            {
+                Responses response = operationHandler.ExecuteOperation(operation);
+                return null;
+            }
+        }
+
+        private Operation ParseCommand(string command)
+        {
+            if (command.Equals(null))
+            {
+                return null;
+            }
+            else
+            {
+                CommandParser CP = new CommandParser();
+                return CP.ParseOperation(command);
+            }
+        }
+
+        /*
+        private string ValidateCommand(string command)
+        {
+            if (!command.Equals(null) & command.Length == 0)
+            {
+                return null;
+            }
+            else
+            {
+                CommandStack.Push(command);
+                return command;
+            }
+        } */
+    }
+}
---
+++ ToDo++/OperationHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace ToDo
+{
+    public class OperationHandler
+    {
+        private TaskList taskList;
+        
+        public OperationHandler()
+        {
+             
+        }
+
+        //Need to take in an instance of Operation to execute
+        public Responses ExecuteOperation(Operation operation)
+        {
+            try
+            {
+                Task taskToAdd = operation.GetTask();
+                taskList.Add(taskToAdd);
+               
+                WriteXML();
+
+                return Responses.ADD_SUCCESS;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+                return Responses.ERROR;
+            }
+
+        }
+
+        public void WriteXML()
+        {
+
+            // use reflection to get all derived types
+            var knownTypes = Assembly.GetExecutingAssembly().GetTypes().Where(
+                t => typeof(TaskList).IsAssignableFrom(t) || typeof(
+                TaskFloating).IsAssignableFrom(t) || typeof(TaskDeadline).IsAssignableFrom(t)).ToArray();
+
+            // prepare to serialize a car object
+            XmlSerializer writer = new XmlSerializer(typeof(TaskList), knownTypes);
+
+            //System.Xml.Serialization.XmlSerializer writer =
+            //    new System.Xml.Serialization.XmlSerializer(typeof(TaskList));
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(
+                @"C:\Users\Raaj\Desktop\SerializationOverview.xml");
+            writer.Serialize(file, taskList);
+            file.Close();
+        }
+    }
+
+}
