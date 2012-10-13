ce5840f8d93c427328bfd820bb704c49b4eac954
Raaj <Raaj@Raaj-PC>
2012-10-13 17:25:59 +0800

    Instance of Logic made and implemented in input

---
+++ ParsingLogicUnitTest/CommandParserTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo;
+
+namespace CommandParserTest
+{
+    [TestClass]
+    public class CommandParserTest
+    {
+        CommandParser testParser = new CommandParser();
+
+        [TestMethod]
+        public void OperationParseTest()
+        {
+            testParser.ParseOperation("task do stuff add by 9 pm");
+            return;
+        }
+
+        /*
+        [TestMethod]
+        public void IndexSortTest()
+        {
+            List<int[]> testIndexes = new List<int[]>();
+            testIndexes.Add(new int[2] { 6, 10 });
+            testIndexes.Add(new int[2] { 0, 4 });
+            testIndexes.Add(new int[2] { 3, 7 });
+            testIndexes.Add(new int[2] { 1, 2 });
+            List<int[]> expectedResult = new List<int[]>{
+                new int[2] { 0, 4 },
+                new int[2] { 1, 2 },
+                new int[2] { 3, 7 },
+                new int[2] { 6, 10 }
+            };            
+            testParser.SortIndexes(ref testIndexes);
+            CollectionAssert.AreEqual(
+                testIndexes.SelectMany(x => x).ToList(),
+                expectedResult.SelectMany(x => x).ToList()
+                );
+        }
+
+        [TestMethod]
+        public void IndexRemoveTest()
+        {
+            List<int[]> testIndexes = new List<int[]>();
+            testIndexes.Add(new int[2] { 0, 4 });
+            testIndexes.Add(new int[2] { 1, 2 });
+            testIndexes.Add(new int[2] { 3, 7 });
+            testIndexes.Add(new int[2] { 6, 10 });
+            List<int[]> expectedResult = new List<int[]>{
+                new int[2] { 0, 4 },
+                new int[2] { 3, 7 }
+            };
+            testParser.RemoveBadIndexes(ref testIndexes);
+            CollectionAssert.AreEqual(
+                testIndexes.SelectMany(x => x).ToList(),
+                expectedResult.SelectMany(x => x).ToList()
+                );
+        }
+        */
+    }
+}
---
+++ ToDo++/UI/OutputBox.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ToDo
+{
+    class OutputBox: RichTextBox
+    {
+        private SettingsManager settingsManager;
+
+        public void SetSettingsManager(SettingsManager passedSettingsManager) { settingsManager = passedSettingsManager; }
+
+        public void LoadSettingsIntoOutput()
+        {
+            this.SetOutputSize(settingsManager.GetTextSize());
+        }
+
+        #region TextSizeControl
+
+        public void SetOutputSize(int size)
+        {
+            this.SelectAll();
+            var family = this.SelectedText.ToString();
+            this.SelectionFont = new Font(family, size);
+            this.DeselectAll();
+        }
+
+        public void DecreaseSizeOfOutput()
+        {
+            settingsManager.DecreaseTextSize();
+            this.SetOutputSize(settingsManager.GetTextSize());
+        }
+
+        public void IncreaseSizeOfOutput()
+        {
+            settingsManager.IncreaseTextSize();
+            this.SetOutputSize(settingsManager.GetTextSize());
+        }
+
+        #endregion
+
+        #region FormattingControl
+
+        //Set Formatting for your Text
+        public void SetFormat(Color color, string text, int size)
+        {
+            RichTextBox box = this;
+            int start = box.TextLength;
+            box.AppendText(text);
+            int end = box.TextLength;
+
+            box.Select(start, end - start + 1);
+            box.SelectionColor = color;
+            box.SelectionFont = new Font("Tahoma", size);
+            box.SelectionLength = 0;
+        }
+
+        #endregion
+
+        //Append the Output Window
+        public void DisplayCommand(string userInput,string systemOutput)
+        {
+            SetFormat(Color.Blue, "User: ", 8);
+            SetFormat(Color.Black, userInput, 8);
+            SetFormat(Color.Red, "\n", 8);
+            SetFormat(Color.Red, "ToDo++: ", 8);
+            SetFormat(Color.Black, systemOutput, 8);
+            SetFormat(Color.Red, "\n", 8);
+            this.ScrollToCaret();
+        }
+
+    }
+}
