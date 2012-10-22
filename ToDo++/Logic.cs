bfcb1e78c1957fd4d574b452b4199c22b1a8537e
Ivan <ravenxce@gmail.com>
2012-10-22 14:11:59 +0800

    Storage class: finished xml handling for task list. todo: settings xml.

---
+++ ToDo++/Logic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class Logic
+    {
+        OperationHandler operationHandler;
+        CommandParser commandParser;
+        StringParser stringParser;
+        Storage storage;
+        List<Task> taskList;
+  
+        public Logic()
+        {
+            storage = new Storage("testfile.xml", "testsettings.xml");
+            operationHandler = new OperationHandler(storage);
+            stringParser = new StringParser();
+            commandParser = new CommandParser(ref stringParser);
+            taskList = storage.LoadTasksFromFile();
+        }
+
+        public string ProcessCommand(string input)
+        {
+            Operation operation = ParseCommand(input);
+            return ExecuteCommand(operation);
+        }
+
+        public bool UpdateSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExecuteCommand(Operation operation)
+        {
+            string response;
+            response = operationHandler.Execute(operation, ref taskList);
+            return response;
+        }
+
+        private Operation ParseCommand(string command)
+        {
+            if (command.Equals(null))
+            {
+                return null;
+            }
+            else
+            {
+                Operation derivedOperation = commandParser.ParseOperation(command);
+                return derivedOperation;
+            }
+        }
+    }
+ 
+}
