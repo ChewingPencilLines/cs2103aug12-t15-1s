e74c3354e9f9b50b0902db4a6bd7e921824a562e
RavenXce <RavenXce@RavenXce-NB.nus.edu.sg>
2012-10-25 12:33:45 +0800

    @raaj: moved exceptions file to be shared by all. removed some comparators fixed invalid datetime exception

--- ToDo++/CustomExceptions.cs
+++ ToDo++/CustomExceptions.cs
@@ -9,4 +9,5 @@ namespace ToDo
     public class TextSizeOutOfRangeException : Exception { public TextSizeOutOfRangeException(string message) : base(message) { } }
     public class RepeatCommandException : Exception { public RepeatCommandException(string message) : base(message) { } }
     public class NothingSelectedException : Exception { public NothingSelectedException(string message) : base(message) { } }
+    public class InvalidDateTimeException : Exception { public InvalidDateTimeException(string message) : base(message) { } }
 }
