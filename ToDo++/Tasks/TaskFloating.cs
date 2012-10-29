2a26f5d33eb6e19fb12f9797cee9fcd65ebd8d18
Jenna <Jenna@Jenna-HP>
2012-10-29 20:31:34 +0800

    Corrected mess up of tdeadline, floating and event task classes -_-

--- ToDo++/Tasks/TaskFloating.cs
+++ ToDo++/Tasks/TaskFloating.cs
@@ -6,37 +6,19 @@ using System.Xml.Linq;
 
 namespace ToDo
 {
-    class TaskEvent : Task
+    class TaskFloating : Task
     {
-        private DateTime endTime;
-        public DateTime EndTime
-        {
-            get { return endTime; }
-            //set { endTime = value; }
-        }
-
-        private DateTime startTime;
-        public DateTime StartTime
-        {
-            get { return startTime; }
-            //set { startTime = value; }
-        }
-
-        public TaskEvent(string taskName, DateTime startTime, DateTime endTime, Boolean isDone = false, int forceID = -1)
+        public TaskFloating(string taskName, Boolean isDone = false, int forceID = -1)
             : base(taskName, isDone, forceID)
         {
-            this.startTime = startTime;
-            this.endTime = endTime;
         }
 
         public override XElement ToXElement()
         {
             XElement task = new XElement("Task",
                             new XAttribute("id", id.ToString()),
-                            new XAttribute("type", "Event"),
+                            new XAttribute("type", "Floating"),
                             new XElement("Name", taskName),
-                            new XElement("StartTime", startTime.ToString()),
-                            new XElement("EndTime", endTime.ToString()),
                             new XElement("State", doneState.ToString())
                             );
             return task;
