f12860209bce866dcf0125320a44747a53a3019c
Ivan <ravenxce@gmail.com>
2012-10-30 11:39:08 +0800

    Fixed issue 53. unhandled exception when load task file.

---
+++ ToDo++/GlobalEventHandlers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public static class EventHandlers
+    {
+        public static event EventHandler StayOnTopHandler;
+        public static void StayOnTop(bool status){ StayOnTopHandler(status, EventArgs.Empty);}
+
+        public static event EventHandler UpdateSettingsHandler;
+        public static void UpdateSettings(SettingInformation settingsList) { UpdateSettingsHandler(settingsList, EventArgs.Empty); }
+    }
+}
---
+++ ToDo++/Operations/OperationDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo
+{
+    class OperationDisplay : Operation
+    {
+        public OperationDisplay()
+        { }
+
+        public override string Execute(List<Task> taskList, Storage storageXML)
+        {
+            this.storageXML = storageXML;
+            string response;
+            // int numOfMatches;
+            // return Search(out numOfMatches, taskList, "");            
+            response = GenerateDisplayString(taskList);
+            return response;
+        }
+    }
+}
