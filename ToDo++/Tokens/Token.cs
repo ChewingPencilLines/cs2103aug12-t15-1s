7d92b25014cd06e72a0a2a26aed0dae727209521
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-01 14:23:17 +0800

    add defensive codes

---
+++ ToDo++/Tokens/Token.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public abstract class Token
+    {
+        static private List<CommandType> rangeableCommandTypes
+            = new List<CommandType> { CommandType.DELETE, CommandType.DONE, CommandType.MODIFY, CommandType.POSTPONE };
+
+        private int position;
+        internal Token(int position)
+        {
+            this.position = position;
+        }  
+        internal int Position
+        {
+            get { return position; }           
+        }
+
+        internal abstract void UpdateAttributes(OperationAttributes attrb);
+
+        internal bool RequiresRange()
+        {
+            if(this.GetType() == typeof(TokenCommand))
+            {
+                TokenCommand token = (TokenCommand)this;
+                if (rangeableCommandTypes.Contains(token.Value))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
