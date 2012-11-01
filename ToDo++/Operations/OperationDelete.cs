0f811104e803f3086e8e94c3fd5d4597858e422e
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-01 14:23:17 +0800

    add defensive codes

--- ToDo++/Operations/OperationDelete.cs
+++ ToDo++/Operations/OperationDelete.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ToDo
 {
@@ -63,6 +64,7 @@ namespace ToDo
             }
             else
             {
+                Debug.Assert(endindex >= index);
                 if (endindex == index)
                 {
                     Task taskToDelete = lastListedTasks[index.Value];
