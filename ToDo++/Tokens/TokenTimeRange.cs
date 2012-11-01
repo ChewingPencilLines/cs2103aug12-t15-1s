b5a540dfbba698079647c1e3c14bd34a5af48084
Jenna <Jenna@Jenna-HP>
2012-11-02 00:40:09 +0800

    Renamed TokenRange to TokenIndexRange and GenerateRangeTokens to GenerateIndexRangeTokens Created TokenTimeRange class Set up TimeRangeKeywords dictionary Added isTimeRange regex for detection of time ranges by keywords (hours, days, months)

---
+++ ToDo++/Tokens/TokenTimeRange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenTimeRange : Token
+    {
+        TimeRangeKeywordsType timeRange;
+        TimeRangeType timeRangeType;
+        int index;
+
+        internal int Value
+        {
+            get { return index; }
+        }
+        internal TimeRangeType Type
+        {
+            get { return timeRangeType; }
+        }
+        internal TimeRangeKeywordsType Range
+        {
+            get { return timeRange; }
+        }
+        internal TokenTimeRange(int position, int val, TimeRangeType type, TimeRangeKeywordsType range)
+            : base(position)
+        {
+            index = val;
+            timeRangeType = type;
+            timeRange = range;
+        }
+
+        internal override void UpdateAttributes(OperationAttributes attrb)
+        {
+            if (index != null)
+            {
+                attrb.rangeIndexes[0] = index;
+            }
+            attrb.timeRangeType = timeRangeType;
+            attrb.timeRange = timeRange;
+        }
+    }
+}
