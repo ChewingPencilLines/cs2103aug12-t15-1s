c20302e356cbbccbc9ab27f25fecfc7227840504
Raaj <Raaj@Raaj-PC>
2012-11-05 17:41:07 +0800

    Font Settings Added

---
+++ ToDo++/Parsers/CommandParser.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+[assembly: InternalsVisibleTo("ParsingLogicUnitTest")]
+
+namespace ToDo
+{
+    class CommandParser
+    {
+        const int START_INDEX = 0;
+        const int END_INDEX = 1;
+        StringParser stringParser;
+
+        public CommandParser(ref StringParser stringParser)
+        {
+            this.stringParser = stringParser;
+        }
+
+        public Operation ParseOperation(string input)
+        {
+            // Get position of delimiters so we can treat those substrings as a single word.
+            List<Token> tokens = stringParser.ParseStringIntoTokens(input);
+            return GenerateOperation(tokens);            
+        }
+
+        private static Operation GenerateOperation(List<Token> tokens)
+        {            
+            OperationAttributes opAttributes = new OperationAttributes();
+            foreach (Token token in tokens)
+            {
+                try
+                {
+                    token.UpdateAttributes(opAttributes);
+                }
+                catch (MultipleCommandsException)
+                {
+                    WarnUserOfMultipleCommands();
+                }
+            }
+            if (opAttributes.commandType == CommandType.SCHEDULE)
+            {
+                opAttributes.SetScheduleTime();
+            }
+            else
+            {
+                opAttributes.SetSearchTime();
+            }
+            opAttributes.CombineDateTimes();
+            Operation newOperation = CreateOperation(opAttributes);
+            return newOperation;
+        }
+
+        // Create operation based on derived values, and whether they have been used.
+        private static Operation CreateOperation(OperationAttributes opAttributes)
+        {            
+            CommandType commandType = opAttributes.commandType;
+            DateTime? startCombined = opAttributes.startDateTime;
+            DateTime? endCombined = opAttributes.endDateTime;
+            DateTimeSpecificity isSpecific = opAttributes.isSpecific;
+            bool isAll = opAttributes.rangeIsAll;
+            string taskName = opAttributes.taskName;
+            int[] taskIndex = opAttributes.rangeIndexes;
+            int timeRangeIndex = opAttributes.timeRangeIndex;
+            TimeRangeType timeRangeType = opAttributes.timeRangeType;
+            SortType sortType = opAttributes.sortType;
+
+            Task task;
+            Operation newOperation = null;
+            switch (commandType)
+            {
+                case CommandType.ADD:
+                    task = GenerateNewTask(taskName, startCombined, endCombined, isSpecific);
+                    newOperation = new OperationAdd(task);
+                    break;
+                case CommandType.DELETE:
+                    newOperation = new OperationDelete(taskName, taskIndex, startCombined, endCombined, isSpecific, isAll);
+                    break;
+                case CommandType.DISPLAY:
+                    newOperation = new OperationDisplayDefault();
+                    break;
+                case CommandType.MODIFY:
+                    task = GenerateNewTask(taskName, startCombined, endCombined, isSpecific);
+                    newOperation = new OperationModify(taskIndex,task);
+                    break;
+                case CommandType.SEARCH:
+                    newOperation = new OperationSearch(taskName, startCombined, endCombined, isSpecific);
+                    break;
+                case CommandType.SORT:
+                    newOperation = new OperationSort(sortType);
+                    break; 
+                case CommandType.REDO:
+                    newOperation = new OperationRedo();
+                    break;
+                case CommandType.UNDO:
+                    newOperation = new OperationUndo();
+                    break;
+                case CommandType.DONE:
+                    newOperation = new OperationMarkAsDone(taskName,taskIndex,startCombined);
+                    break;
+                case CommandType.POSTPONE:
+                    newOperation = new OperationPostpone(taskName, taskIndex, startCombined, endCombined, isSpecific, isAll);
+                    break;
+                case CommandType.SCHEDULE:
+                    newOperation = new OperationSchedule(taskName, (DateTime)startCombined, endCombined, isSpecific, timeRangeIndex, timeRangeType);
+                    break;
+                case CommandType.EXIT:
+                    System.Environment.Exit(0);
+                    break;
+            }
+            return newOperation;
+        }
+
+        private static Task GenerateNewTask(
+            string taskName,
+            DateTime? startTime,
+            DateTime? endTime,
+            DateTimeSpecificity isSpecific
+            )
+        {
+            if (startTime == null && endTime == null)
+                return new TaskFloating(taskName);
+            else if (startTime == null && endTime != null)
+                return new TaskDeadline(taskName, (DateTime)endTime, isSpecific);
+            else if (startTime != null && endTime == null)
+            {
+                // If endTime is not specified set endTime based on startTime.
+                endTime = startTime;
+                if (!isSpecific.StartTime)
+                {
+                    endTime = ((DateTime)endTime).AddDays(1);
+                    endTime = ((DateTime)endTime).AddMinutes(-1);
+                }
+                return new TaskEvent(taskName, (DateTime)startTime, (DateTime)startTime, isSpecific);
+            }
+            else
+                return new TaskEvent(taskName, (DateTime)startTime, (DateTime)endTime, isSpecific);
+        }
+
+        private static void WarnUserOfMultipleCommands()
+        {
+            AlertBox.Show("Invalid input.\r\nMultiple commands were entered.");
+            throw new NotImplementedException("Multiple commands were issued. Functionality NYI.");
+        }  
+    }
+}
---
+++ ToDo++/Tasks/TaskEvent.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ToDo
+{
+    class TaskEvent : Task
+    {
+        public DateTimeSpecificity isSpecific;
+
+        private DateTime endDateTime;
+        public DateTime EndDateTime
+        {
+          //  set { endTime = value; }
+            get { return endDateTime; }
+        }
+
+        private DateTime startDateTime;
+        public DateTime StartDateTime
+        {
+          //  set { startTime = value; }
+            get { return startDateTime; }         
+        }
+
+        public TaskEvent(
+            string taskName,
+            DateTime startTime,
+            DateTime endTime,
+            DateTimeSpecificity isSpecific,
+            Boolean isDone = false,
+            int forceID = -1)
+            : base(taskName, isDone, forceID)
+        {
+            this.startDateTime = startTime;
+            this.endDateTime = endTime;
+            this.isSpecific = isSpecific;
+        }
+
+        public override XElement ToXElement()
+        {
+            XElement task = new XElement("Task",
+                            new XAttribute("id", id.ToString()),
+                            new XAttribute("type", "Event"),
+                            new XElement("Name", taskName),
+                            new XElement("StartTime", startDateTime.ToString()),
+                            new XElement("EndTime", endDateTime.ToString()),
+                            isSpecific.ToXElement<DateTimeSpecificity>(),
+                            new XElement("Done", doneState.ToString())
+                            );
+            return task;
+        }
+
+        public override DayOfWeek GetDay()
+        {
+            return startDateTime.DayOfWeek;
+        }
+
+        public override bool IsWithinTime(DateTimeSpecificity compareIsSpecific, DateTime? start, DateTime? end)
+        {
+            bool isWithinTime = true;
+            DateTime startCompare, endCompare;
+
+            // Start search
+            if (start != null)
+            {
+                startCompare = (DateTime)start;
+
+                // If comparision is not specific to Day/Month, extend search range
+                if ((!isSpecific.StartDate.Day && !isSpecific.StartTime ||
+                    (!compareIsSpecific.StartDate.Day && !compareIsSpecific.StartTime)))
+                {                    
+                    if (!isSpecific.StartDate.Month)
+                        startCompare = new DateTime(startCompare.Year, 1, 1);
+                    else
+                        startCompare = new DateTime(startCompare.Year, startCompare.Month, 1);
+                }
+
+                if (startDateTime < startCompare)
+                    isWithinTime = false;
+            }
+            if (end != null)
+            {
+                endCompare = (DateTime)end;
+
+                // Extend compare range if task dates are not specific                
+                if ((!isSpecific.EndDate.Day && !isSpecific.EndTime ||
+                    (!compareIsSpecific.EndDate.Day && !compareIsSpecific.EndTime)))
+                {
+                    if (!isSpecific.EndDate.Month)
+                        endCompare = new DateTime(endCompare.Year + 1, 1, 1);
+                    else
+                    {
+                        endCompare = endCompare.AddMonths(1);
+                        endCompare = new DateTime(endCompare.Year, endCompare.Month, 1);
+                    }
+                    endCompare = endCompare.AddMinutes(-1);
+                }
+                else if (!compareIsSpecific.EndTime && compareIsSpecific.StartDate.Day == true)
+                {
+                    endCompare = new DateTime(endCompare.Year, endCompare.Month, endCompare.Day, 23, 59, 0);
+                }
+
+                if (endDateTime > endCompare) 
+                    isWithinTime = false;
+            }
+            return isWithinTime;
+        }
+
+        public override string GetTimeString()
+        {
+            string timeString = "";
+            
+            if (isSpecific.StartDate.Day) timeString += startDateTime.ToString("d ");
+            timeString += startDateTime.ToString("MMM");
+            if (startDateTime.Year != DateTime.Now.Year) timeString += " " + startDateTime.Year;
+            if (isSpecific.StartTime) timeString += ", " + startDateTime.ToShortTimeString();
+
+            if (startDateTime != EndDateTime)
+            {
+                timeString += " -- ";
+                if (StartDateTime.Date != EndDateTime.Date)
+                {
+                    if (isSpecific.EndDate.Day) timeString += endDateTime.ToString("d ");
+                    timeString += endDateTime.ToString("MMM");
+                    if (endDateTime.Year != DateTime.Now.Year) timeString += " " + endDateTime.Year;
+                    if (isSpecific.EndTime) timeString += ", ";
+                }
+                if (isSpecific.EndTime) timeString +=  endDateTime.ToShortTimeString();
+            }
+            return timeString;
+        }
+
+        public string test()
+        {
+            return String.Empty;
+        }
+
+        public override Task Postpone(DateTime? NewDate)
+        {
+            TaskEvent result;
+            if (NewDate == null)
+                result= new TaskEvent(this.taskName, this.startDateTime.AddDays(1),this.endDateTime.AddDays(1), this.isSpecific, this.doneState);           
+            else
+            {
+                DateTime NewEnd = this.endDateTime + (NewDate.Value - this.startDateTime);
+                result = new TaskEvent(this.taskName, NewDate.Value, NewEnd, this.isSpecific, this.doneState);  
+            }
+            return result;
+        }
+    
+    }
+}
