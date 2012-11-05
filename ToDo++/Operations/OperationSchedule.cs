8620e1d0a020e20c174dfc517c927e1f3fc4c265
Ivan <ravenxce@gmail.com>
2012-11-05 21:24:47 +0800

    Fix undo/redo for add,delete,done. Was never implemented right! going to implement a MarkAsUndone.

---
+++ ToDo++/Operations/OperationSchedule.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class OperationSchedule : Operation
+    {
+        string taskName;
+        DateTime startDateTime;
+        DateTime? endDateTime;
+        DateTimeSpecificity isSpecific;
+        int timeRangeIndex;
+        TimeRangeType timeRangeType;
+        
+        public OperationSchedule(string taskName, DateTime startDateTime, DateTime? endDateTime, DateTimeSpecificity isSpecific, int timeRangeIndex, TimeRangeType timeRangeType)
+        {
+            this.taskName = taskName;
+            this.startDateTime = startDateTime;
+            this.endDateTime = endDateTime;
+            this.isSpecific = isSpecific;
+            this.timeRangeIndex = timeRangeIndex;
+            this.timeRangeType = timeRangeType;
+        }
+        public override Response Execute(List<Task> taskList, Storage storageIO)
+        {
+            // default response: failure to schedule task
+            Response response = null;
+            // if there is no time span indicated i.e. 3 days etc., get default 
+            // getting task duration
+            if (timeRangeIndex == 0 && timeRangeType == TimeRangeType.DEFAULT)
+            {
+                // todo: get default time range from settings
+                // for now, just take it to be 1 hour long
+                timeRangeIndex = 1;
+                timeRangeType = TimeRangeType.HOUR;
+            }
+            // todo: check that range > span, else return failure response
+            // (error response should be different from if no fitting slot can be found)
+            bool isTimeSpanSpecified = isSpecific.StartTime && isSpecific.EndTime;
+            if (endDateTime == null)
+            {
+                if (isSpecific.StartDate.Day)
+                {
+                    endDateTime = startDateTime.AddDays(1);
+                }
+                else
+                {
+                    endDateTime = startDateTime.AddMonths(1);
+                }
+            }
+            if (!isTimeSpanSpecified)
+            {
+                startDateTime = startDateTime.AddHours(DateTime.Now.AddHours(1).Hour);
+            }
+            switch (timeRangeType)
+            {
+                case TimeRangeType.HOUR:
+                    int numberOfConsecutiveHours = ((DateTime)endDateTime).Hour - startDateTime.Hour;
+                    if (timeRangeIndex > numberOfConsecutiveHours)
+                    {
+                        // error
+                    }
+                    break;
+                case TimeRangeType.DAY:
+                    int numberOfConsecutiveDays = ((DateTime)endDateTime).Day - startDateTime.Day;
+                    if (timeRangeIndex > numberOfConsecutiveDays)
+                    {
+                        //error
+                    }
+                    break;
+                case TimeRangeType.MONTH:
+                    if (startDateTime.AddMonths(timeRangeIndex) > ((DateTime)endDateTime))
+                    {
+                        // error
+                    }
+                    break;
+                default:
+                    // should never fall to this
+                    break;
+            }
+            // todo: loop through all tasks to find earliest possible fitting time
+            bool isSlotFound = false;
+            int index = 1;
+            DateTime tryStartTime = new DateTime();
+            DateTime tryEndTime = new DateTime();
+            DateTimeSpecificity searchSpecificity = new DateTimeSpecificity();
+            while (!isSlotFound && tryEndTime <= ((DateTime)endDateTime))
+            {
+                tryStartTime = startDateTime.AddHours((index-1)*timeRangeIndex);
+                switch (timeRangeType)
+                {
+                    case TimeRangeType.HOUR:
+                        tryEndTime = startDateTime.AddHours(index*timeRangeIndex);
+                        break;
+                    case TimeRangeType.DAY:
+                        tryEndTime = startDateTime.AddDays(index*timeRangeIndex);
+                        break;
+                    case TimeRangeType.MONTH:
+                        tryEndTime = startDateTime.AddMonths(index*timeRangeIndex);
+                        break;
+                }
+                List<Task> searchResults = SearchForTasks(taskList, null, searchSpecificity, false, tryStartTime, tryEndTime);
+                // once fitting time is found, change its start and end datetime then
+                // add the task; return success response
+                if (searchResults.Count == 0)
+                {
+                    TaskEvent newTask = new TaskEvent(taskName, tryStartTime, tryEndTime, searchSpecificity);
+                    response = AddTask(newTask, taskList);
+                    if (response.IsSuccessful())
+                    {
+                        TrackOperation();
+                    }
+                    isSlotFound = true;
+                }
+                index++;
+            }
+            return response;
+        }
+        public override Response Undo(List<Task> taskList, Storage storageIO)
+        {
+            Task task = undoTask.Pop();
+            return DeleteTask(task, taskList);
+        }
+    }
+}
---
+++ ToDo++/Settings/Settings.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections.Generic;
+
+namespace ToDo
+{
+    public class Settings
+    {
+        // ******************************************************************
+        // Static Keyword Declarations
+        // ******************************************************************
+
+        static SettingInformation settingInfo;
+
+        public Settings()
+        {
+            InitializeSettings();
+        }
+
+        // ******************************************************************
+        // Initialization and opening of settings file
+        // ******************************************************************
+
+        private void InitializeSettings()
+        {
+            settingInfo = new SettingInformation();
+        }
+
+        public void UpdateSettings(SettingInformation updatedInfo)
+        {
+            settingInfo = updatedInfo;
+            CustomDictionary.UpdateDictionary(settingInfo.userCommandKeywords,settingInfo.userContextKeywords);
+        }
+
+        // ******************************************************************
+        // Getters/Setters
+        // ******************************************************************
+
+        #region GettersSetters
+
+        public void SetTextSize(int size) { settingInfo.misc.TextSize = size; EventHandlers.UpdateSettings(settingInfo); }
+        public int GetTextSize() { return settingInfo.misc.TextSize; }
+        public void SetLoadOnStartupStatus(bool status) { settingInfo.misc.LoadOnStartup = status; EventHandlers.UpdateSettings(settingInfo); }
+        public bool GetLoadOnStartupStatus() { return settingInfo.misc.LoadOnStartup; }
+        public void SetStartMinimized(bool status) { settingInfo.misc.StartMinimized = status; EventHandlers.UpdateSettings(settingInfo); }
+        public bool GetStartMinimizeStatus() { return settingInfo.misc.StartMinimized; }
+        public void SetStayOnTop(bool status) { settingInfo.misc.StayOnTop = status; EventHandlers.UpdateSettings(settingInfo); }
+        public bool GetStayOnTopStatus() { return settingInfo.misc.StayOnTop; }
+        public void SetFontSelection(string font) { settingInfo.misc.FontSelection = font; EventHandlers.UpdateSettings(settingInfo); }
+        public string GetFontSelection() { return settingInfo.misc.FontSelection; }
+
+        #endregion;
+
+        // ******************************************************************
+        // Keyword Operations
+        // ******************************************************************
+
+        #region KeywordOperations
+
+        #region CommandKeywords
+
+        /// <summary>
+        /// This method adds a new Command to the list of available commands
+        /// If a command repeats itself, an exception will be thrown
+        /// </summary>
+        /// <param name="newKeyword">New Command that is to be added</param>
+        /// <param name="commandString">Specify to which CommandType it is being added to</param>
+        public void AddCommandKeyword(string newKeyword, CommandType commandType)
+        {
+            if (settingInfo.ContainsCommandKeyword(newKeyword, commandType))
+                throw new RepeatCommandException("There is such a command in the list already");
+            if (settingInfo.userContextKeywords.ContainsKey(newKeyword))
+                throw new RepeatCommandException("There is a repeat keyword already");
+            if (settingInfo.userCommandKeywords.ContainsKey(newKeyword))
+                throw new RepeatCommandException("There is a repeat keyword already ");
+            settingInfo.userCommandKeywords.Add(newKeyword, commandType);
+
+            EventHandlers.UpdateSettings(settingInfo);
+        }
+
+        /// <summary>
+        /// This method removes the specified command
+        /// </summary>
+        /// <param name="commandString">Specify to which CommandType it is being added to</param>
+        public void RemoveCommandKeyword(string keywordToRemove)
+        {
+            if (keywordToRemove == "add" || keywordToRemove == "remove" || keywordToRemove == "display" || keywordToRemove == "sort"
+                || keywordToRemove == "search" || keywordToRemove == "modify" || keywordToRemove == "undo" || keywordToRemove == "redo"
+                || keywordToRemove == "done" || keywordToRemove == "postpone")
+                throw new InvalidDeleteFlexiException("This is a default keyword and can't be removed");
+            settingInfo.userCommandKeywords.Remove(keywordToRemove);
+
+            EventHandlers.UpdateSettings(settingInfo);
+        }
+
+        /// <summary>
+        /// Returns a list of all added/available user commands
+        /// </summary>
+        /// <param name="commandType">Specify the type of Command you wish to see User Commands of</param>
+        /// <returns>Returns a list of added commands</returns>
+        public List<string> GetCommandKeywordList(CommandType commandType)
+        {
+            List<string> getCommands = new List<string>();
+            foreach(var pair in settingInfo.userCommandKeywords){
+                if(pair.Value==commandType)
+                    getCommands.Add(pair.Key);
+            }
+
+            return getCommands;
+        }
+
+        #endregion
+
+        #region ContextKeywords
+
+        /// <summary>
+        /// This method adds a new Command to the list of available commands
+        /// If a command repeats itself, an exception will be thrown
+        /// </summary>
+        /// <param name="newKeyword">New Command that is to be added</param>
+        /// <param name="commandString">Specify to which CommandType it is being added to</param>
+        public void AddContextKeyword(string newKeyword, ContextType contextType)
+        {
+            if (settingInfo.ContainsContextKeyword(newKeyword, contextType))
+                throw new RepeatCommandException("There is such a command in the list already");
+            if (settingInfo.userContextKeywords.ContainsKey(newKeyword))
+                throw new RepeatCommandException("There is a repeat keyword already");
+            if (settingInfo.userCommandKeywords.ContainsKey(newKeyword))
+                throw new RepeatCommandException("There is a repeat keyword already");
+            settingInfo.userContextKeywords.Add(newKeyword, contextType);
+
+            EventHandlers.UpdateSettings(settingInfo);
+        }
+
+        /// <summary>
+        /// This method removes the specified command
+        /// </summary>
+        /// <param name="commandString">Specify to which CommandType it is being added to</param>
+        public void RemoveContextKeyword(string keywordToRemove)
+        {
+            if (keywordToRemove == "on" || keywordToRemove == "from" || keywordToRemove == "to" || keywordToRemove == "-"
+                || keywordToRemove == "this" || keywordToRemove == "next" || keywordToRemove == "following" )
+                    throw new InvalidDeleteFlexiException("This is a default keyword and can't be removed");
+            settingInfo.userContextKeywords.Remove(keywordToRemove);
+
+            EventHandlers.UpdateSettings(settingInfo);
+        }
+
+        /// <summary>
+        /// Returns a list of all added/available user commands
+        /// </summary>
+        /// <param name="contextType">Specify the type of Command you wish to see User Commands of</param>
+        /// <returns>Returns a list of added commands</returns>
+        public List<string> GetContextKeywordList(ContextType contextType)
+        {
+            List<string> getCommands = new List<string>();
+            foreach (var pair in settingInfo.userContextKeywords)
+            {
+                if (pair.Value == contextType)
+                    getCommands.Add(pair.Key);
+            }
+
+            return getCommands;
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
