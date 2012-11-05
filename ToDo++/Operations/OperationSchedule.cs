48b9c46c6b7337184d0c94ac235328397885add3
Jenna <Jenna@Jenna-HP>
2012-11-05 22:01:43 +0800

    Modified opschedule; damn messy right now, cbb; hold on! # of bugs: 1. unable to differentiate between "schedule task today 3 hours", "schedule task 3 days" 2. task name gets eaten up, zzz

--- ToDo++/Operations/OperationSchedule.cs
+++ ToDo++/Operations/OperationSchedule.cs
@@ -28,33 +28,9 @@ namespace ToDo
         {
             // default response: failure to schedule task
             Response response = null;
-            // if there is no time span indicated i.e. 3 days etc., get default 
-            // getting task duration
-            if (timeRangeIndex == 0 && timeRangeType == TimeRangeType.DEFAULT)
-            {
-                // todo: get default time range from settings
-                // for now, just take it to be 1 hour long
-                timeRangeIndex = 1;
-                timeRangeType = TimeRangeType.HOUR;
-            }
-            // todo: check that range > span, else return failure response
+            retrieveParameters();
+            // check that range > span, else return failure response
             // (error response should be different from if no fitting slot can be found)
-            bool isTimeSpanSpecified = isSpecific.StartTime && isSpecific.EndTime;
-            if (endDateTime == null)
-            {
-                if (isSpecific.StartDate.Day)
-                {
-                    endDateTime = startDateTime.AddDays(1);
-                }
-                else
-                {
-                    endDateTime = startDateTime.AddMonths(1);
-                }
-            }
-            if (!isTimeSpanSpecified)
-            {
-                startDateTime = startDateTime.AddHours(DateTime.Now.AddHours(1).Hour);
-            }
             switch (timeRangeType)
             {
                 case TimeRangeType.HOUR:
@@ -65,8 +41,8 @@ namespace ToDo
                     }
                     break;
                 case TimeRangeType.DAY:
-                    int numberOfConsecutiveDays = ((DateTime)endDateTime).Day - startDateTime.Day;
-                    if (timeRangeIndex > numberOfConsecutiveDays)
+                    TimeSpan span = ((DateTime)endDateTime) - startDateTime;
+                    if (timeRangeIndex > span.TotalDays)
                     {
                         //error
                     }
@@ -82,32 +58,44 @@ namespace ToDo
                     break;
             }
             // todo: loop through all tasks to find earliest possible fitting time
-            bool isSlotFound = false;
-            int index = 1;
-            DateTime tryStartTime = new DateTime();
+            bool isSlotFound = false;;
+            DateTime tryStartTime = startDateTime;
             DateTime tryEndTime = new DateTime();
+            DateTime copyTryStartTime = startDateTime;
             DateTimeSpecificity searchSpecificity = new DateTimeSpecificity();
             while (!isSlotFound && tryEndTime <= ((DateTime)endDateTime))
             {
-                tryStartTime = startDateTime.AddHours((index-1)*timeRangeIndex);
+                int numberOfSetsToLoop = 1;
                 switch (timeRangeType)
                 {
                     case TimeRangeType.HOUR:
-                        tryEndTime = startDateTime.AddHours(index*timeRangeIndex);
+                        tryStartTime = tryStartTime.AddHours(1);
+                        tryEndTime = tryStartTime.AddHours(timeRangeIndex);
                         break;
                     case TimeRangeType.DAY:
-                        tryEndTime = startDateTime.AddDays(index*timeRangeIndex);
+                        copyTryStartTime = tryStartTime;
+                        numberOfSetsToLoop = timeRangeIndex;
                         break;
                     case TimeRangeType.MONTH:
-                        tryEndTime = startDateTime.AddMonths(index*timeRangeIndex);
+                        TimeSpan span = tryStartTime.AddMonths(timeRangeIndex) - tryStartTime;
+                        copyTryStartTime = tryStartTime;
+                        numberOfSetsToLoop = (int)span.TotalDays;
                         break;
                 }
-                List<Task> searchResults = SearchForTasks(taskList, null, searchSpecificity, false, tryStartTime, tryEndTime);
+                List<Task> searchResults = new List<Task>();
+                for (int i = 0; i < numberOfSetsToLoop; i++)
+                {
+                    if (tryEndTime <= tryStartTime)
+                        tryEndTime = tryStartTime.AddDays(1).Add(((DateTime)endDateTime).TimeOfDay);
+                    searchResults = SearchForTasks(taskList, null, searchSpecificity, false, tryStartTime, tryEndTime);
+                    if (searchResults.Count != 0) break;
+                    tryStartTime = tryStartTime.AddDays(1);
+                }
                 // once fitting time is found, change its start and end datetime then
                 // add the task; return success response
                 if (searchResults.Count == 0)
                 {
-                    TaskEvent newTask = new TaskEvent(taskName, tryStartTime, tryEndTime, searchSpecificity);
+                    TaskEvent newTask = new TaskEvent(taskName, copyTryStartTime, tryEndTime.AddSeconds(-1), searchSpecificity);
                     response = AddTask(newTask, taskList);
                     if (response.IsSuccessful())
                     {
@@ -115,10 +103,67 @@ namespace ToDo
                     }
                     isSlotFound = true;
                 }
-                index++;
+                tryStartTime = copyTryStartTime.AddDays(1);
             }
             return response;
         }
+
+        private void retrieveParameters()
+        {
+            // if there is no time span indicated i.e. 3 days etc., get default 
+            // getting task duration
+            if (timeRangeIndex == 0 && timeRangeType == TimeRangeType.DEFAULT)
+            {
+                // todo: get default time range from settings
+                // for now, just take it to be 1 hour long
+                timeRangeIndex = CustomDictionary.defaultTimeRangeIndex;
+                timeRangeType = CustomDictionary.defaultTimeRangeType;
+            }
+            // setting the start and end search datetimes
+            if (isSpecific.StartTime && !isSpecific.EndTime)
+            {
+                if (isSpecific.StartDate.Day)
+                {
+                    if (endDateTime == null)
+                        endDateTime = startDateTime.AddDays(1).Date;
+                    else
+                        endDateTime = ((DateTime)endDateTime).AddDays(1).Date;
+                }
+                else
+                {
+                    if (endDateTime == null)
+                        endDateTime = startDateTime.AddMonths(1).Date;
+                    else
+                    {
+                        if (isSpecific.EndDate.Day)
+                            endDateTime = ((DateTime)endDateTime).AddDays(1).Date;
+                        else
+                            endDateTime = startDateTime.AddMonths(1).Date;
+                    }
+                }
+            }
+            else if (!isSpecific.StartTime && !isSpecific.EndTime)
+            {
+                startDateTime = startDateTime.AddHours(DateTime.Now.AddHours(1).Hour);
+                if (endDateTime != null)
+                {
+                    if (isSpecific.EndDate.Day)
+                        endDateTime = ((DateTime)endDateTime).AddDays(1).Date;
+                    else
+                        endDateTime = ((DateTime)endDateTime).AddMonths(1).Date;
+                }
+                else
+                {
+                    // shit: not able to differentiate between "schedule task today 3 hours" and
+                    // "schedule task 3 days" -- endDateTime = DateTime.Max;
+                    if (isSpecific.StartDate.Day)
+                        endDateTime = startDateTime.AddDays(1).Date;
+                    else
+                        endDateTime = startDateTime.AddMonths(1).Date;
+                }
+            }
+        }
+
         public override Response Undo(List<Task> taskList, Storage storageIO)
         {
             Task task = undoTask.Pop();
