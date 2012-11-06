6fb4be2eb49430f3803104fa6ebdb26889ca48cf
Jenna <Jenna@Jenna-HP>
2012-11-06 22:47:26 +0800

    fixed time range night bug and some schedule bugs

---
+++ ToDo++/UI/Components/TopMenuControl.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    public partial class TopMenuControl : UserControl
+    {
+
+        private UI ui;
+        bool collaspedState = false;
+        bool isShowingSettings = false;
+        
+        /// <summary>
+        /// Currently sets the Text Size of the OutputBox
+        /// </summary>
+        public void InitializeWithUI(UI ui)
+        {
+            this.ui = ui;
+        }
+
+
+        public TopMenuControl()
+        {
+            InitializeComponent();
+        }
+       
+        private void updownButton_Click(object sender, EventArgs e)
+        {
+            if (collaspedState == false)
+            {
+                ui.ToggleCollapsedState();
+                collaspedState = true;
+            }
+            else
+            {
+                ui.ToggleCollapsedState();                
+                collaspedState = false;
+            }
+        }
+
+        public void SetUpDownButton(bool isCollasped)
+        {
+            if(isCollasped)
+                updownButton.Image = Properties.Resources.downButton;
+            else
+                updownButton.Image = Properties.Resources.upButton;
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+            ui.MinimiseMaximiseTray();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            ui.Exit();
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            if (isShowingSettings == false)
+            {
+                ui.SwitchToSettingsPanel();
+                if (collaspedState == true)
+                    ui.ToggleCollapsedState();
+                isShowingSettings = true;
+                collaspedState = false;
+            }
+            else
+            {
+                ui.SwitchToToDoPanel();
+                isShowingSettings = false;
+            }
+        }
+
+        private void questionButton_MouseEnter(object sender, EventArgs e)
+        {
+            questionButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            questionButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+        }
+
+
+    }
+}
