e9c2c44874e246edd47ed6ffad6c9f4ea5775bbf
Raaj <Raaj@Raaj-PC>
2012-11-07 00:29:26 +0800

    Implemented Time Range

---
+++ ToDo++/Tokens/TokenLiteral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenLiteral : Token
+    {
+        string literal;
+
+        internal TokenLiteral(int position, string val)
+            : base(position)
+        {
+            literal = val;
+        }
+
+        internal override void UpdateAttributes(OperationAttributes attrb)
+        {
+            attrb.taskName = literal;
+        }
+    }
+}
