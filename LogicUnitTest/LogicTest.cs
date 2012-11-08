78d43894991fd450c51a48dfa4c6c80099c8dd9c
Jenna <Jenna@Jenna-HP>
2012-11-09 01:22:35 +0800

    Fixed failure of detection of "TMR" (in caps)

---
+++ LogicUnitTest/LogicTest.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using System.Linq;
+using System.Collections.Generic; 
+using ToDo;
+
+namespace LogicUnitTest
+{
+    [TestClass]
+    public class LogicTest
+    {
+        Logic logic = new Logic();
+
+        [TestMethod]
+        public void BasicAddTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            result = logic.ProcessCommand("add milk");
+            Assert.AreEqual("Added new task \"milk\" successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void DeleteByNameTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add abcd");
+            result = logic.ProcessCommand("delete abcd");
+            Assert.AreEqual("Deleted task \"abcd\" successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void DeleteAllTest()
+        {
+            Response result;
+            logic.ProcessCommand("add aa");
+            logic.ProcessCommand("add aaa");
+            result = logic.ProcessCommand("delete all");
+            Assert.AreEqual("Deleted all tasks successfully.", result.FeedbackString);
+            return;
+        } 
+
+        [TestMethod]
+        public void DeleteByNameMultipleResultTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aa");
+            logic.ProcessCommand("add aaa");
+            result = logic.ProcessCommand("delete a");
+            Assert.AreEqual("Displaying all tasks matching \"a\".", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void DeleteByNameFailTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aa");
+            logic.ProcessCommand("add aaa");
+            result = logic.ProcessCommand("delete bb");
+            Assert.AreEqual("No matching tasks found!", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void DeleteByIndexTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add cd");
+            logic.ProcessCommand("add cde");
+            logic.ProcessCommand("delete c");
+            result = logic.ProcessCommand("delete 1");
+            Assert.AreEqual("Deleted task \"cd\" successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void SearchNameTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aaa");
+            result = logic.ProcessCommand("search aaa");
+            Assert.AreEqual("Displaying all tasks matching \"aaa\".", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void SearchDateTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aaa 5pm");
+            logic.ProcessCommand("add bbb 7pm");
+            result = logic.ProcessCommand("search today");
+            Assert.AreEqual("Displaying all tasks with time constraints.", result.FeedbackString);
+           // Assert.AreEqual("aaa", result.TasksToBeDisplayed[0].TaskName);
+            //Assert.AreEqual("bbb", result.TasksToBeDisplayed[1].TaskName);
+            return;
+        }
+
+        [TestMethod]
+        public void SearchDateSpecifyTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aba 5pm");
+            result = logic.ProcessCommand("search 5pm");
+            Assert.AreEqual("Displaying all tasks with time constraints.", result.FeedbackString);
+          //  Assert.AreEqual("aba", result.TasksToBeDisplayed[0].TaskName);
+            return;
+        }
+
+        [TestMethod]
+        public void DisplayEmptyTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            result = logic.ProcessCommand("display");
+            Assert.AreEqual("Displaying all tasks.", result.FeedbackString); 
+            return;
+        }
+
+        //no feedback known since broken of postpone
+        [TestMethod]
+        public void PostponeByDateTest()
+        {
+            Response result; 
+            logic.ProcessCommand("add pp 5pm");
+            logic.ProcessCommand("add qq 3pm");
+            result = logic.ProcessCommand("postpone today tmr");
+            Assert.AreEqual("Postponed task \"pp\" successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void PostponeByNameTest()
+        {
+            Response result;
+            logic.ProcessCommand("add pp 5pm");
+            result = logic.ProcessCommand("postpone pp jan");
+            Assert.AreEqual("Postponed task \"pp\" successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void ModifyByIndexTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add bbbb tmr");
+            result = logic.ProcessCommand("modify 1 aaaa");
+            Assert.AreEqual("Modified task successfully.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void MarkTaskTest()
+        {
+            Response result;
+            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("add aa");
+            result = logic.ProcessCommand("done aa");
+            Assert.AreEqual("Successfully marked \"aa\" as done.", result.FeedbackString);
+            return;
+        }
+
+        [TestMethod]
+        public void SortByNameTest()
+        {
+            Response result; 
+            result = logic.ProcessCommand("sort name");
+            Assert.AreEqual("Sorting by Name.", result.FeedbackString);
+            return;
+        }
+    }
+}
---
+++ ToDo++/UI/Components/TopMenuControl.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    public partial class TopMenuControl : UserControl
+    {
+
+        private UI ui;
+        bool collaspedState = false;
+        bool isShowingSettings = false;
+        
+        /// <summary>
+        /// Currently sets the Text Size of the OutputBox
+        /// </summary>
+        public void InitializeWithUI(UI ui)
+        {
+            questionButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            questionButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            settingsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            settingsButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            updownButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            updownButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            minButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            minButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            closeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            closeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.ui = ui;
+        }
+
+
+        public TopMenuControl()
+        {
+            InitializeComponent();
+        }
+       
+        private void updownButton_Click(object sender, EventArgs e)
+        {
+            CollapseExpandFunction();
+        }
+
+        public void SetUpDownButton(bool isCollasped)
+        {
+            if(isCollasped)
+                updownButton.Image = Properties.Resources.downButton;
+            else
+                updownButton.Image = Properties.Resources.upButton;
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+            ui.MinimiseMaximiseTray();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            ui.Exit();
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+            if (isShowingSettings == false)
+            {
+                ui.SwitchToSettingsPanel();
+                if (collaspedState == true)
+                    ui.ToggleCollapsedState();
+                isShowingSettings = true;
+                collaspedState = false;
+            }
+            else
+            {
+                ui.SwitchToToDoPanel();
+                isShowingSettings = false;
+            }
+        }
+
+        public void CollapseExpandToDo()
+        {
+            CollapseExpandFunction();
+        }
+
+        private void CollapseExpandFunction()
+        {
+            if (collaspedState == false)
+            {
+                ui.ToggleCollapsedState();
+                collaspedState = true;
+            }
+            else
+            {
+                ui.ToggleCollapsedState();
+                collaspedState = false;
+            }
+        }
+
+        private void questionButton_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+    }
+}
