8b2a4b38630ffed5fe0d7553ed099e4db9bda03a
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-09 12:00:57 +0800

    fix some bugs in test

--- LogicUnitTest/LogicTest.cs
+++ LogicUnitTest/LogicTest.cs
@@ -15,6 +15,7 @@ namespace LogicUnitTest
         public void BasicAddTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             result = logic.ProcessCommand("add milk");
             Assert.AreEqual("Added new task \"milk\" successfully.", result.FeedbackString);
@@ -25,6 +26,7 @@ namespace LogicUnitTest
         public void DeleteByNameTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add abcd");
             result = logic.ProcessCommand("delete abcd");
@@ -36,6 +38,7 @@ namespace LogicUnitTest
         public void DeleteAllTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("add aa");
             logic.ProcessCommand("add aaa");
             result = logic.ProcessCommand("delete all");
@@ -47,6 +50,7 @@ namespace LogicUnitTest
         public void DeleteByNameMultipleResultTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aa");
             logic.ProcessCommand("add aaa");
@@ -59,7 +63,7 @@ namespace LogicUnitTest
         public void DeleteByNameFailTest()
         {
             Response result;
-            logic.ProcessCommand("delete all");
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aa");
             logic.ProcessCommand("add aaa");
@@ -72,12 +76,15 @@ namespace LogicUnitTest
         public void DeleteByIndexTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add cd");
             logic.ProcessCommand("add cde");
             logic.ProcessCommand("delete c");
             result = logic.ProcessCommand("delete 1");
             Assert.AreEqual("Deleted task \"cd\" successfully.", result.FeedbackString);
+            result = logic.ProcessCommand("delete -1");
+            Assert.AreEqual("No matching tasks found!", result.FeedbackString);
             return;
         }
 
@@ -85,6 +92,7 @@ namespace LogicUnitTest
         public void SearchNameTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aaa");
             result = logic.ProcessCommand("search aaa");
@@ -96,11 +104,12 @@ namespace LogicUnitTest
         public void SearchDateTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aaa 5pm");
             logic.ProcessCommand("add bbb 7pm");
             result = logic.ProcessCommand("search today");
-            Assert.AreEqual("Displaying all tasks with time constraints.", result.FeedbackString);
+            Assert.AreEqual("Displaying all tasks within 2012/11/9 0:00 to 2012/11/9 0:00.", result.FeedbackString);
            // Assert.AreEqual("aaa", result.TasksToBeDisplayed[0].TaskName);
             //Assert.AreEqual("bbb", result.TasksToBeDisplayed[1].TaskName);
             return;
@@ -110,10 +119,11 @@ namespace LogicUnitTest
         public void SearchDateSpecifyTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aba 5pm");
             result = logic.ProcessCommand("search 5pm");
-            Assert.AreEqual("Displaying all tasks with time constraints.", result.FeedbackString);
+            Assert.AreEqual("Displaying all tasks within 2012/11/9 17:00.", result.FeedbackString);
           //  Assert.AreEqual("aba", result.TasksToBeDisplayed[0].TaskName);
             return;
         }
@@ -122,6 +132,7 @@ namespace LogicUnitTest
         public void DisplayEmptyTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             result = logic.ProcessCommand("display");
             Assert.AreEqual("Displaying all tasks.", result.FeedbackString); 
@@ -132,7 +143,9 @@ namespace LogicUnitTest
         [TestMethod]
         public void PostponeByDateTest()
         {
-            Response result; 
+            Response result;
+            logic.ProcessCommand("display");
+            logic.ProcessCommand("delete all");
             logic.ProcessCommand("add pp 5pm");
             logic.ProcessCommand("add qq 3pm");
             result = logic.ProcessCommand("postpone today tmr");
@@ -144,6 +157,8 @@ namespace LogicUnitTest
         public void PostponeByNameTest()
         {
             Response result;
+            logic.ProcessCommand("display");
+            logic.ProcessCommand("delete all");
             logic.ProcessCommand("add pp 5pm");
             result = logic.ProcessCommand("postpone pp jan");
             Assert.AreEqual("Postponed task \"pp\" successfully.", result.FeedbackString);
@@ -154,6 +169,7 @@ namespace LogicUnitTest
         public void ModifyByIndexTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add bbbb tmr");
             result = logic.ProcessCommand("modify 1 aaaa");
@@ -165,6 +181,7 @@ namespace LogicUnitTest
         public void MarkTaskTest()
         {
             Response result;
+            logic.ProcessCommand("display");
             logic.ProcessCommand("delete all");
             logic.ProcessCommand("add aa");
             result = logic.ProcessCommand("done aa");
@@ -177,7 +194,7 @@ namespace LogicUnitTest
         {
             Response result; 
             result = logic.ProcessCommand("sort name");
-            Assert.AreEqual("Sorting by Name.", result.FeedbackString);
+            Assert.AreEqual("Sorting by name.", result.FeedbackString);
             return;
         }
     }
