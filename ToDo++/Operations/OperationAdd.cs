7d7350a744e2519754417fdb36402029348272b7
RavenXce <RavenXce@RavenXce-NB.nus.edu.sg>
2012-11-09 15:55:02 +0800

    Fixed bugs relating to: - null response - postpone Refactoring for Operation.

---
+++ ToDo++/Operations/OperationAdd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ToDo
+{
+    public class OperationAdd : Operation
+    {
+        private Task newTask;
+        public OperationAdd(Task setTask)
+        {
+            newTask = setTask;
+        }
+        public override Response Execute(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response;
+            if (newTask == null)
+            {
+                return new Response(Result.FAILURE, Format.DEFAULT, this.GetType());
+            }
+            response = AddTask(newTask);
+            if (response.IsSuccessful())
+            {
+                TrackOperation();
+            }
+            return response;
+        }
+
+        public override Response Undo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response = DeleteTask(newTask);
+            return response;
+        }
+
+        public override Response Redo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response = AddTask(newTask);
+            return response;
+        }
+    }
+}
---
+++ ToDo++/Tokens/TokenLiteral.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenLiteral : Token
+    {
+        string literal;
+
+        internal TokenLiteral(int position, string val)
+            : base(position)
+        {
+            literal = val;
+        }
+
+        internal override void UpdateAttributes(OperationGenerator attrb)
+        {
+            if (attrb.taskName == null)
+            {
+                attrb.taskName = literal;
+            }
+        }
+    }
+}
