b4644a14c30623dd70187d703accb6dccccb6dc5
Raaj <Raaj@Raaj-PC>
2012-11-10 19:41:08 +0800

    Help!

---
+++ ToDo++/Operations/OperationAdd.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ToDo
+{
+    public class OperationAdd : Operation
+    {
+        private Task newTask;
+        public OperationAdd(Task setTask, SortType sortType)
+            : base(sortType)
+        {
+            newTask = setTask;
+        }
+        public override Response Execute(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response;
+            if (newTask == null)
+            {
+                return new Response(Result.FAILURE, sortType, this.GetType());
+            }
+            response = AddTask(newTask);
+            if (response.IsSuccessful())
+            {
+                TrackOperation();
+            }
+            return response;
+        }
+
+        public override Response Undo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response = DeleteTask(newTask);
+            return response;
+        }
+
+        public override Response Redo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response = AddTask(newTask);
+            return response;
+        }
+    }
+}
---
+++ ToDo++/SerializationExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Runtime.Serialization;
+
+namespace ToDo
+{
+    public static class SerializationExtensions
+    {
+        public static string Serialize<T>(this T obj)
+        {
+            var serializer = new DataContractSerializer(obj.GetType());
+            using (var writer = new StringWriter())
+            using (var stm = new XmlTextWriter(writer))
+            {
+                serializer.WriteObject(stm, obj);         
+                string check = writer.ToString();
+                return writer.ToString();
+            }
+        }
+        public static T Deserialize<T>(this string serialized)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            using (var reader = new StringReader(serialized))
+            using (var stm = new XmlTextReader(reader))
+            {
+                return (T)serializer.ReadObject(stm);
+            }
+        }
+
+        public static XElement ToXElement<T>(this object obj)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (TextWriter streamWriter = new StreamWriter(memoryStream))
+                {
+                    XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+                    namespaces.Add(string.Empty, string.Empty);
+                    var xmlSerializer = new XmlSerializer(typeof(T));
+                    xmlSerializer.Serialize(streamWriter, obj, namespaces);
+                    return XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray()));                    
+                }
+            }
+        }
+
+        public static T FromXElement<T>(this XElement xElement)
+        {            
+            using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(xElement.ToString())))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(memoryStream);
+            }
+        }
+    }
+}
