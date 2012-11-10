ea4300f157ecaa22e86eb388a80cecfd45d6b53f
Ivan <ravenxce@gmail.com>
2012-11-11 01:21:38 +0800

    Logged storage controller.

---
+++ ToDo++/Parsers/CommandParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+[assembly: InternalsVisibleTo("ParsingLogicUnitTest")]
+
+namespace ToDo
+{
+    class CommandParser
+    {
+        StringParser stringParser;
+
+        public CommandParser(StringParser stringParser)
+        {
+            this.stringParser = stringParser;
+        }
+
+        public Operation ParseOperation(string input)
+        {
+            List<Token> tokens = stringParser.ParseStringIntoTokens(input);
+            return GenerateOperation(tokens);       
+        }
+
+        private static Operation GenerateOperation(List<Token> tokens)
+        {            
+            // reset factory configuration / make new factory.
+            OperationGenerator factory = new OperationGenerator();
+            foreach (Token token in tokens)
+            {
+               token.ConfigureGenerator(factory);
+            }
+            // implement? ReleaseUnusedTokens();
+            factory.SetTimes();
+            Operation newOperation = factory.CreateOperation();
+            return newOperation;
+        }
+
+    }
+}
---
+++ ToDo++/Tokens/TokenTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace ToDo
+{
+    public class TokenTime : Token
+    {
+        TimeSpan time;
+        Boolean specific;
+
+        internal TokenTime(int position, TimeSpan val, Boolean specific_flag)
+            : base(position)
+        {
+            time = val;
+            specific = specific_flag;
+            Logger.Info("Created an time token object", "TokenTime::TokenTime");
+        }
+
+        internal override void ConfigureGenerator(OperationGenerator attrb)
+        {
+            switch (attrb.currentMode)
+            {
+                case ContextType.STARTTIME:
+                    attrb.StartTimeOnly = time;
+                    attrb.isSpecific.StartTime = specific;
+                    Logger.Info("Updated StartTimeOnly and its specificity.", "ConfigureGenerator::TokenTime");
+                    break;
+                case ContextType.ENDTIME:
+                    attrb.SetConditionalEndTime(time, specific);
+                    Logger.Info("Successfully set conditional end time.", "ConfigureGenerator::TokenTime");
+                    break;
+                case ContextType.DEADLINE:
+                    attrb.EndTimeOnly = time;
+                    attrb.isSpecific.EndTime = specific;
+                    Logger.Info("Updated EndTimeOnly and its specificity.", "ConfigureGenerator::TokenTime");
+                    break;
+                default:
+                    Logger.Warning("Fell through switch statement.", "ConfigureGenerator::TokenTime");
+                    Debug.Assert(false, "Fell through switch statement in ConfigureGenerator, TokenTime!");
+                    break;
+            }
+        }
+    }
+}
