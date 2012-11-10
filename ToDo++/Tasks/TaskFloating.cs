df0f892dc4666494e35def3c4caaa63e55c981b5
Jenna <Jenna@Jenna-HP>
2012-11-10 17:13:06 +0800

    uhm, added some shortcuts... sorry :S but i really needed those :S damn annoying

---
+++ ToDo++/Tasks/TaskFloating.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ToDo
+{
+    public class TaskFloating : Task
+    {
+        public TaskFloating(string taskName, Boolean isDone = false, int forceID = -1)
+            : base(taskName, isDone, forceID)
+        {
+            Logger.Info("Created an floating task", "TaskFloating::TaskFloating");
+        }
+
+        public override XElement ToXElement()
+        {
+            XElement task = new XElement("Task",
+                            new XAttribute("id", id.ToString()),
+                            new XAttribute("type", "Floating"),
+                            new XElement("Name", taskName),
+                            new XElement("Done", doneState.ToString())
+                            );
+            return task;
+        }
+
+        public override bool IsWithinTime(DateTime? start, DateTime? end)
+        {
+            return false;
+        }
+        
+        public override void CopyDateTimes(ref DateTime? startTime, ref DateTime? endTime, ref DateTimeSpecificity specific)
+        {
+            startTime = null;
+            endTime = null;
+            specific = new DateTimeSpecificity();
+        }
+    }
+}
