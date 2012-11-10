9513e78f33286c3fd3cc5494548086fdc7b8d898
Raaj <Raaj@Raaj-PC>
2012-11-11 01:55:19 +0800

    Fixed Bugs

--- ToDo++/Properties/Resources.Designer.cs
+++ ToDo++/Properties/Resources.Designer.cs
@@ -439,5 +439,15 @@ namespace ToDo.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] user {
+            get {
+                object obj = ResourceManager.GetObject("user", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
