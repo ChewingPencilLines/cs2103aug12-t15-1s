97e8e3fe0c7a46af6b9098cc47faaf80db46f2d7
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-11 05:04:19 +0800

    add more tests for parser

--- ParsingLogicUnitTest/StringParserUnitTest.cs
+++ ParsingLogicUnitTest/StringParserUnitTest.cs
@@ -25,6 +25,47 @@ namespace StringParserTest
             Assert.IsTrue(result[3] is TokenLiteral);
             return;
         }
+
+        [TestMethod]
+        public void ParseSortCommandTest()
+        {
+            input = "sort    name";
+            result = testStrParser.ParseStringIntoTokens(input);
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result[0] is TokenCommand);
+            Assert.IsTrue(result[1] is TokenSortType);
+            return;
+        }
+
+        [TestMethod]
+        public void AddComplicatedDatedTaskTest1()
+        {
+            input = "buy milk tmr 3 am to 12/12/12 5 pm add";
+            result = testStrParser.ParseStringIntoTokens(input);
+            Assert.AreEqual(7, result.Count);
+            Assert.IsTrue(result[0] is TokenLiteral);
+            Assert.IsTrue(result[1] is TokenDay);
+            Assert.IsTrue(result[2] is TokenTime); 
+            Assert.IsTrue(result[3] is TokenContext);
+            Assert.IsTrue(result[4] is TokenDate);
+            Assert.IsTrue(result[5] is TokenTime);
+            Assert.IsTrue(result[6] is TokenCommand);
+            return;
+        }
+
+        [TestMethod]
+        public void AddComplicatedDatedTaskTest2()
+        {
+            input = "add buy - milk jan 23rd 2016 to feb 29th 2016";
+            result = testStrParser.ParseStringIntoTokens(input);
+            Assert.AreEqual(5, result.Count);
+            Assert.IsTrue(result[0] is TokenCommand);
+            Assert.IsTrue(result[1] is TokenLiteral);
+            Assert.IsTrue(result[2] is TokenDate);
+            Assert.IsTrue(result[3] is TokenContext);
+            Assert.IsTrue(result[4] is TokenDate);
+            return;
+        }
     }
 }
 
