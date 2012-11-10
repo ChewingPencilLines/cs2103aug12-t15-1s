fedea680cf70f5da1de581b2386ee0f639e64610
Jenna <Jenna@Jenna-HP>
2012-11-10 16:19:40 +0800

    documentation; user manual & diagrams

---
+++ ToDo++/Operations/OperationModify.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ToDo
+{
+    class OperationModify : Operation
+    {
+        // ******************************************************************
+        // Parameters
+        // ******************************************************************
+
+        #region Parameters
+        private string taskName;
+        private int startIndex;
+        private int endIndex;
+        private bool hasIndex;
+        private bool isAll;
+        private SearchType searchType;
+        private DateTime? startTime = null, endTime = null;
+        private DateTimeSpecificity isSpecific;
+        #endregion
+
+        #region Task Tracking
+        private Task oldTask;
+        private Task newTask;
+        #endregion
+
+        // ******************************************************************
+        // Constructors
+        // ******************************************************************
+
+        #region Constructors
+        public OperationModify(string taskName, int[] indexRange, DateTime? startTime,
+            DateTime? endTime, DateTimeSpecificity isSpecific, bool isAll, SearchType searchType, SortType sortType)
+            : base(sortType)
+        {
+            if (indexRange == null) hasIndex = false;
+            else
+            {
+                hasIndex = true;
+                this.startIndex = indexRange[TokenIndexRange.START_INDEX] - 1;
+                this.endIndex = indexRange[TokenIndexRange.END_INDEX] - 1;
+            }
+            if (taskName == null) this.taskName = "";
+            else this.taskName = taskName;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.isSpecific = isSpecific;
+            this.isAll = isAll;
+            this.searchType = searchType;
+        }
+        #endregion
+
+        /*
+        *  If a user keys in nothing or only index or only task details
+        *  after the commandtype, then all tasks will be shown.
+        *  only when user input an index + information will a task be modified.
+        */
+        public override Response Execute(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+
+            Response response = null;
+
+            if (!hasIndex)
+            {
+                // search for task
+                SetMembers(taskList, storageIO);
+                List<Task> searchResults = SearchForTasks(taskName, isSpecific, false, startTime, endTime, searchType);
+                currentListedTasks = new List<Task>(searchResults);
+
+                string[] criteria;
+                SetArgumentsForFeedbackString(out criteria, taskName, startTime, endTime, searchType);
+
+                return new Response(Result.SUCCESS, sortType, typeof(OperationSearch), currentListedTasks, criteria);
+            }
+            else
+            {                
+                response = CheckIfIndexesAreValid(startIndex, endIndex);
+                if (response != null) return response;
+
+                if (startIndex != endIndex || isAll == true)
+                {
+                    return new Response(Result.INVALID_TASK, sortType, this.GetType());
+                }
+
+                oldTask = currentListedTasks[startIndex];
+                if (taskName == null || taskName == String.Empty)
+                {
+                    // copy over taskName from indexed task if didn't specify a name
+                    taskName = oldTask.TaskName;
+                }
+                else if (startTime == null && endTime == null)
+                {
+                    // copy over all times from indexed task
+                    oldTask.CopyDateTimes(ref startTime, ref endTime, ref isSpecific);
+                }
+                newTask = Task.GenerateNewTask(taskName, startTime, endTime, isSpecific);
+
+                response = ModifyTask(oldTask, newTask);
+            }
+
+            if (response.IsSuccessful())
+            {
+                TrackOperation();
+            }
+            return response;
+        }
+
+        protected Response ModifyTask(Task taskToModify, Task newTask)
+        {
+            Response response = null;
+            response = DeleteTask(taskToModify);
+            if (response.IsSuccessful()) response = AddTask(newTask);
+            if (response.IsSuccessful())
+                return new Response(Result.SUCCESS, sortType, typeof(OperationModify), currentListedTasks);
+            else
+                return response;
+        }
+
+        public override Response Undo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+            Response response = ModifyTask(newTask, oldTask);
+            return response;
+        }
+
+        public override Response Redo(List<Task> taskList, Storage storageIO)
+        {
+            SetMembers(taskList, storageIO);
+            Response response = ModifyTask(oldTask, newTask);
+            return response;
+        }
+    }
+}
---
+++ ToDo++/UI/Components/PreferencesControllers/FontColorSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    public partial class FontColorSettings : UserControl
+    {
+        Settings settings;
+
+        public void InitializeFontColorControl(Settings settings)
+        {
+            this.settings = settings;
+        }
+
+        public FontColorSettings()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textSizeButton_Click(object sender, EventArgs e)
+        {
+            FontBox.InitializeOptions(settings.GetFontSelection(), settings.GetTextSize(), Color.White);
+            FontBox.Show(true, true, false);
+            if (FontBox.ConfirmHit())
+            {
+                settings.SetTextSize(FontBox.GetSize());
+                settings.SetFontSelection(FontBox.GetFont());
+                EventHandlers.UpdateUI();
+            }
+        }
+
+        private void taskDoneColorButton_Click(object sender, EventArgs e)
+        {
+            FontBox.InitializeOptions(settings.GetFontSelection(), settings.GetTextSize(), settings.GetTaskDoneColor());
+            FontBox.Show(false, false, true);
+            if (FontBox.ConfirmHit())
+            {
+                settings.SetTaskDoneColor(FontBox.GetColor());
+                EventHandlers.UpdateUI();
+            }
+        }
+
+        private void taskDeadlineColorButton_Click(object sender, EventArgs e)
+        {
+            FontBox.InitializeOptions(settings.GetFontSelection(), settings.GetTextSize(), settings.GetTaskMissedDeadlineColor());
+            FontBox.Show(false, false, true);
+            if (FontBox.ConfirmHit())
+            {
+                settings.SetTaskMissedDeadlineColor(FontBox.GetColor());
+                EventHandlers.UpdateUI();
+            }
+        }
+
+        private void taskDeadlineDayColor_Click(object sender, EventArgs e)
+        {
+            FontBox.InitializeOptions(settings.GetFontSelection(), settings.GetTextSize(), settings.GetTaskNearingDeadlineColor());
+            FontBox.Show(false, false, true);
+            if (FontBox.ConfirmHit())
+            {
+                settings.SetTaskNearingDeadlineColor(FontBox.GetColor());
+                EventHandlers.UpdateUI();
+            }
+        }
+
+        private void taskEventColor_Click(object sender, EventArgs e)
+        {
+            FontBox.InitializeOptions(settings.GetFontSelection(), settings.GetTextSize(), settings.GetTaskOverColor());
+            FontBox.Show(false, false, true);
+            if (FontBox.ConfirmHit())
+            {
+                settings.SetTaskOverColor(FontBox.GetColor());
+                EventHandlers.UpdateUI();
+            }
+        }
+    }
+}
