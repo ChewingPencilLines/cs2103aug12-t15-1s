91537b7441d98956a99c347f590943dcbd500c0d
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-11 04:37:08 +0800

    add unit test for stringparser internal function

---
+++ ParsingLogicUnitTest/StringParserUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo;
+
+namespace StringParserTest
+{
+    [TestClass]
+    public class StringParserUnitTest
+    {
+        StringParser testStrParser = new StringParser();
+        string input;
+        List<Token> result;
+
+        [TestMethod]
+        public void AddSimpleTimedTaskTest()
+        {
+            input = "add morning 8AM TASK";
+            result = testStrParser.ParseStringIntoTokens(input);
+            Assert.AreEqual(4, result.Count);
+            Assert.IsTrue(result[0] is TokenCommand);
+            Assert.IsTrue(result[1] is TokenTimeRange);
+            Assert.IsTrue(result[2] is TokenTime);
+            Assert.IsTrue(result[3] is TokenLiteral);
+            return;
+        }
+    }
+}
+
