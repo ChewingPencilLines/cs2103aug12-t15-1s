f963503fcb0ef03f0bee4ab711fd46f96ce57f49
JIANGQIANPAN <JIANGQIANPAN@JQP.nus.edu.sg>
2012-11-12 00:31:38 +0800

    revise in test, and some rename of test functions

---
+++ ParsingLogicUnitTest/CommandParserTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDo;
+
+namespace CommandParserTest
+{
+    [TestClass]
+    public class CommandParserTest
+    {
+        CommandParser testCmdParser;
+
+        [TestMethod]
+        public void OperationParseSearchDeadlineTest()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("search by oct 30th");
+            Assert.AreEqual("ToDo.OperationSearch", op1.GetType().ToString());
+            return;
+        }
+
+        [TestMethod]
+        public void OperationParseTestTimedSingle()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("task do stuff add Oct 15 5 am");
+            Assert.AreEqual("ToDo.OperationAdd", op1.GetType().ToString());
+            return;
+        }
+
+        [TestMethod]
+        public void OperationParseTestInvalid()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("add task do stuff  by 20");
+            Assert.AreEqual("ToDo.OperationAdd", op1.GetType().ToString());
+            return;
+        }
+
+        [TestMethod]
+        public void OperationParseTestDay()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("add task do stuff by friday 2pm");
+            Assert.AreEqual("ToDo.OperationAdd", op1.GetType().ToString());
+            return;
+        }
+
+        [TestMethod]
+        public void OperationParseTestTimedDuo()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("task do stuff add Oct 15 5 am to 6am");
+            Assert.AreEqual("ToDo.OperationAdd", op1.GetType().ToString());
+            return;
+        }
+
+        [TestMethod]
+        public void OperationParseTestDeadline()
+        {
+            testCmdParser = new CommandParser();
+            Operation op1 = testCmdParser.ParseOperation("task do stuff add by 9 pm");
+            Assert.AreEqual("ToDo.OperationAdd", op1.GetType().ToString());
+            //Assert.IsTrue(task1.TaskName == "task do stuff");
+            //Assert.IsTrue(task1 is TaskDeadline);
+            //TimeSpan deadline = ((TaskDeadline)task1).EndTime.TimeOfDay;
+            //Assert.IsTrue((deadline.Equals(new TimeSpan(21,0,0))));
+            return;
+        }
+
+    }
+}
---
+++ ToDo++/Tokens/TokenContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenContext : Token
+    {
+        ContextType contextType;
+
+        internal TokenContext(int position, ContextType val)
+            : base(position)
+        {
+            contextType = val;
+            Logger.Info("Created a context token object", "TokenContext::TokenContext");
+        }
+
+        internal override void ConfigureGenerator(OperationGenerator attrb)
+        {
+            if (contextType == ContextType.CURRENT ||
+                contextType == ContextType.NEXT ||
+                contextType == ContextType.FOLLOWING
+                )
+            {
+                attrb.CurrentSpecifier = contextType;
+            }
+            else
+            {
+                attrb.CurrentMode = contextType;
+            }
+        }
+
+        /// <summary>
+        /// Gets a flag indicating if the token accepts a context token at the position
+        /// before it.
+        /// </summary>
+        /// <returns>True if it uses a context token; False if it does not.</returns>
+        internal override bool AcceptsContext()
+        {
+            return true;
+        }
+    }
+}
