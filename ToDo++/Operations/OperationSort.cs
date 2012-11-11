badefe200f6db58a5f7c99d404dc85d9b71663e0
Raaj <Raaj@Raaj-PC>
2012-11-11 18:14:05 +0800

    Bug Fixes

---
+++ ToDo++/Operations/OperationSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToDo
+{
+    class OperationSort : Operation
+    {
+        public OperationSort(SortType sortType)
+            :base (sortType)
+        {
+            this.sortType = sortType;
+        }
+
+        public override Response Execute(List<Task> taskList, Storage storageIO)
+        {
+            this.storageIO = storageIO;
+            Response response;
+            
+                // sorting is done On-The-Fly in TaskListViewControl.
+            if(sortType == SortType.DEFAULT)
+                response = new Response(Result.FAILURE, sortType, this.GetType(), currentListedTasks);
+            else
+                response = new Response(Result.SUCCESS, sortType, this.GetType(), currentListedTasks);
+            return response;
+        }
+    }
+}
