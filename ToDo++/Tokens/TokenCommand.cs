5816c46d69aa1cc0c045161f028a9aefb6202e2f
Jenna <Jenna@Jenna-HP>
2012-11-11 15:17:27 +0800

    Added documentation and blah

---
+++ ToDo++/Tokens/TokenCommand.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenCommand : Token
+    {
+        static private List<CommandType> indexRangeableCommandTypes
+            = new List<CommandType> { CommandType.DELETE, CommandType.DONE, CommandType.UNDONE, CommandType.MODIFY, CommandType.POSTPONE, CommandType.SEARCH };
+
+        static private List<CommandType> timeRangeableCommandTypes
+            = new List<CommandType> { CommandType.SCHEDULE, CommandType.POSTPONE, CommandType.ADD };
+
+        CommandType commandType;
+
+        internal CommandType Value
+        {
+            get { return commandType; }
+        }
+
+        internal TokenCommand(int position, CommandType val)
+            : base(position)
+        {
+            commandType = val;
+            Logger.Info("Created a command token object", "TokenCommand::TokenCommand");
+        }
+
+        internal override void ConfigureGenerator(OperationGenerator attrb)
+        {
+            if (attrb.commandType != CommandType.INVALID)
+            {
+                if (Value == CommandType.DONE)
+                {
+                    attrb.searchType = SearchType.DONE;
+                    Logger.Info("Updated SearchType to DONE.", "ConfigureGenerator::TokenCommand");
+                }
+                else if (Value == CommandType.UNDONE)
+                {
+                    attrb.searchType = SearchType.UNDONE;
+                    Logger.Info("Updated SearchType to UNDONE.", "ConfigureGenerator::TokenCommand");
+                }
+                else if (attrb.commandType == CommandType.SORT)
+                {
+                    attrb.commandType = Value;
+                    Logger.Info("Resolved multiple commands to not use Sort as command (lower priority)", "ConfigureGenerator::TokenCommand");
+                }
+                else if (Value == CommandType.SORT)
+                {
+                    Logger.Info("Resolved multiple commands to not use Sort as command (lower priority)", "ConfigureGenerator::TokenCommand");
+                }
+                else
+                {
+                    Logger.Error("Multiple commands detected", "ConfigureGenerator::TokenCommand");
+                    throw new MultipleCommandsException();
+                }
+            }
+            else
+            {
+                attrb.commandType = Value;
+                Logger.Info("commandType is INVALID", "ConfigureGenerator::TokenCommand");
+            }
+        }
+
+        /// <summary>
+        /// This method checks if the command is of a type that accepts index ranges i.e. delete
+        /// </summary>
+        /// <returns>True if positive; false if otherwise</returns>
+        internal bool RequiresIndexRange()
+        {
+            if (indexRangeableCommandTypes.Contains(Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This method checks if the command is of a type that accepts time ranges i.e. schedule
+        /// </summary>
+        /// <returns>True if positive; false if otherwise</returns>
+        internal bool RequiresTimeRange()
+        {
+            if (timeRangeableCommandTypes.Contains(Value))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
