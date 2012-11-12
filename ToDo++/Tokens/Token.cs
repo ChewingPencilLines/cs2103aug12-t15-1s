9b4b571315826d7ec5e0e3ff087bcec53652d7b1
Raaj <Raaj@Raaj-PC.comp.nus.edu.sg>
2012-11-12 16:58:37 +0800

    My Doc

---
+++ ToDo++/Tokens/Token.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public abstract class Token
+    {
+        // Position of the Token within a string.
+        private int position;
+        internal Token(int position)
+        {
+            this.position = position;
+        }  
+        internal int Position
+        {
+            get { return position; }           
+        }
+
+        /// <summary>
+        /// The base method which should be overriden by derived classes.
+        /// It allows the token to configure an OperationGenerator to create
+        /// an appropriate Operation.
+        /// </summary>
+        /// <param name="attrb">The OperationGenerator to configure.</param>
+        internal abstract void ConfigureGenerator(OperationGenerator attrb);
+        
+        /// <summary>
+        /// Gets a flag indicating if the token accepts a context token at the position
+        /// before it.
+        /// </summary>
+        /// <returns>True if it uses a context token; False if it does not.</returns>
+        internal virtual bool AcceptsContext()
+        {
+            return false;
+        }
+    }
+}
