2ff2362f5cf54c734cfea548fe325fa1b2478084
Ivan <ravenxce@gmail.com>
2012-11-12 21:21:35 +0800

    Summary completion. ToMerge.

---
+++ ToDo++/Program.cs
@@ -0,0 +1,25 @@
+﻿//@alice
+using System;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Logger.Info("Starting Application...", "Main");
+            Logic logic = new Logic();
+            Application.Run(new UI(logic));
+            Logger.Info("Application terminated!\r\n", "Main");         
+        }
+
+    }
+}
