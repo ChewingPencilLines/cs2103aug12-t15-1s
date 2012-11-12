439ba0e75415b449ee0213e7f2905d6c2a98a25a
Jenna <Jenna@Jenna-HP>
2012-11-12 19:44:17 +0800

    updating dev manual

---
+++ ToDo++/Tokens/TokenLiteral.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenLiteral : Token
+    {
+        string literal;
+
+        internal TokenLiteral(int position, string val)
+            : base(position)
+        {
+            literal = val;
+            Logger.Info("Created a literal token", "TokenLiteral::TokenLiteral");
+        }
+
+        internal override void ConfigureGenerator(OperationGenerator attrb)
+        {
+            if (attrb.TaskName == null)
+            {
+                attrb.TaskName = literal;
+                Logger.Info("Found task name", "ConfigureGenerator::TokenLiteral");
+            }
+            else
+            {
+                attrb.TaskName += " " + literal;
+                Logger.Warning("Task name already defined but more literals present. Appending to task name.", "ConfigureGenerator::TokenLiteral");
+            }
+        }
+    }
+}
