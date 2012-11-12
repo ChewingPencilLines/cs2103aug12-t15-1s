a9031c8f730e4ea8260f60250d69cfc91f1c5890
Jenna <Jenna@Jenna-HP>
2012-11-12 18:59:56 +0800

    merged project manual

---
+++ ToDo++/Tokens/TokenIndexRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class TokenIndexRange : Token
+    {
+        public const int START_INDEX = 0;
+        public const int END_INDEX = 1;
+        public const int RANGE = 2;
+        int[] indexes;
+        bool isAll;
+
+        internal bool IsAll
+        {
+            get { return isAll; }
+        }
+        internal int[] Value
+        {
+            get { return indexes; }
+        }
+        internal TokenIndexRange(int position, int[] val, bool isAll)
+            : base(position)
+        {
+            indexes = val;
+            this.isAll = isAll;
+            Logger.Info("Created an index range token object", "TokenIndexRange::TokenIndexRange");
+        }
+
+        internal override void ConfigureGenerator(OperationGenerator attrb)
+        {
+            if (indexes != null)
+            {
+                attrb.TaskRangeIndex = indexes;
+            }
+            attrb.RangeIsAll = isAll;
+        }
+    }
+}
