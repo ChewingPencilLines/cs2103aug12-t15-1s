cc869e8ac28dc520adaa6088965b9339f5f32e69
Ivan <ravenxce@gmail.com>
2012-11-13 19:01:03 +0800

    Catch all unhandled exceptions

--- ToDo++/Program.cs
+++ ToDo++/Program.cs
@@ -17,7 +17,20 @@ namespace ToDo
             Application.SetCompatibleTextRenderingDefault(false);
             Logger.Info("Starting Application...", "Main");
             Logic logic = new Logic();
-            Application.Run(new UI(logic));
+            try
+            {
+                Application.Run(new UI(logic));
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                AlertBox.Show("Missing some DLL files!");
+                Logger.Error(e, "Main::Program");
+            }
+            catch (Exception e)
+            {
+                AlertBox.Show(e.ToString());
+                Logger.Error(e, "Main::Program");
+            }
             Logger.Info("Application terminated!\r\n", "Main");         
         }
     }
